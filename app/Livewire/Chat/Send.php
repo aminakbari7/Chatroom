09cfc866df595f6e640559484aedb8d84c47bdd2
Amin <aminakbari733@gmail.com>
2024-11-29 10:50:09 +0330

    Create Send.php

---
+++ app/Livewire/Chat/Send.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Chat;
+
+use Livewire\Component;
+
+class Send extends Component
+{
+    public function render()
+    {
+        return view('livewire.chat.send');
+    }
+}
