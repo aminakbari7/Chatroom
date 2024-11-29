cde2fa819ba6d0588865d1c8213d93d4490d95a0
Amin <aminakbari733@gmail.com>
2024-11-29 10:49:58 +0330

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,24 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Livewire\Chat\ChatBox;
+use App\Livewire\Chat\ChatList;
+use App\Livewire\Chat\Send;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+// chat livewire
+Route::get('/Chat/Send', [Send::class]);
+Route::get('/Chat/ChatList', [ChatList::class]);
+Route::get('/Chat/ChatBox', [ChatBox::class]);
+
+
+
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
